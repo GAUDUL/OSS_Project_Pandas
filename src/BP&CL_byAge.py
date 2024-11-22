8037af88c4eff8c1e39b6dde9b359d1f71561e00
GAUDUL <limjsu12@naver.com>
2024-11-22 18:53:19 +0900

    Disease Count Graph by age

---
+++ src/BP&CL_byAge.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+file_path1='./data/Diseases_Symptoms.csv'
+file_path2='./data/Medicine_Details.csv'
+file_path3='./data/Disease_symptom_and_patient_profile_dataset.csv'
+
+dis_df=pd.read_csv(file_path1) #질병 데이터셋
+medi_df=pd.read_csv(file_path2) #약 데이터셋
+patients_df = pd.read_csv(file_path3) #환자 데이터셋 (증상: cough, fever, fatigue, difficulty breathing)
+
+# Age구간 나누기
+bins = [10, 19, 29, 39, 49, 59, 69, 79, 89, 99]  # 구간 정의
+labels = ['10~19', '20~29', '30~39', '40~49', '50~59', '60~69', '70~79', '80~89', '90~99']  # 구간 레이블
+patients_df['Age'] = pd.cut(patients_df['Age'], bins=bins, labels=labels, right=True)
+
+
+#문자열로 작성돼있는 값들을 수치화 하기 위한 폼, Low는 1, Normal은 2, High는 3으로 치환
+bp_mapping = {'Low': 1, 'Normal': 2, 'High': 3}
+chol_mapping = {'Low': 1, 'Normal': 2, 'High': 3}
+
+#Blood Pressure와 Cholesterol Level에 위의 폼 적용
+patients_df['Blood Pressure'] = patients_df['Blood Pressure'].map(bp_mapping)
+patients_df['Cholesterol Level'] = patients_df['Cholesterol Level'].map(chol_mapping)
+
+# 각 나이대별로 혈압과 콜레스테롤 평균 계산
+age_group_avg = patients_df.groupby('Age', observed=False)[['Blood Pressure', 'Cholesterol Level']].mean().reset_index()
+
+# 선형 그래프 그리기
+plt.figure(figsize=(10, 6))
+
+# Blood Pressure와 Cholesterol Level 선형 그래프
+sns.lineplot(data=age_group_avg, x='Age', y='Blood Pressure', label='Blood Pressure', marker='o')
+sns.lineplot(data=age_group_avg, x='Age', y='Cholesterol Level', label='Cholesterol Level', marker='s')
+
+# 그래프, 나이가 증가할수록 혈압, 콜레스테롤 수치 증가하는 것 확인 가능
+plt.title('Average Blood Pressure and Cholesterol Level by Age')
+plt.xlabel('Age')
+plt.ylabel('Blood Pressure and Cholesterol Level')
+plt.legend()
+plt.grid(True)
+
+plt.show()
---
+++ src/DiseaseCount_byAge.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+file_path1='./data/Diseases_Symptoms.csv'
+file_path2='./data/Medicine_Details.csv'
+file_path3='./data/Disease_symptom_and_patient_profile_dataset.csv'
+
+dis_df=pd.read_csv(file_path1) #질병 데이터셋
+medi_df=pd.read_csv(file_path2) #약 데이터셋
+patients_df = pd.read_csv(file_path3) #환자 데이터셋 (증상: cough, fever, fatigue, difficulty breathing)
+
+
+# Age구간 나누기
+bins = [10, 19, 29, 39, 49, 59, 69, 79, 89, 99]  # 구간 정의
+labels = ['10~19', '20~29', '30~39', '40~49', '50~59', '60~69', '70~79', '80~89', '90~99']  # 구간 레이블
+patients_df['Age'] = pd.cut(patients_df['Age'], bins=bins, labels=labels, right=True)
+
+
+#각 나이대별 질병 빈도와 환자 수 계산
+dis_counts_by_age = patients_df.groupby(['Age', 'Disease']).size().reset_index(name='Count')
+patients_counts_by_age = patients_df.groupby('Age').size().reset_index(name='PatientCount')
+
+# dis_counts_by_age와 patients_counts_by_age 병합
+dis_counts_by_age = pd.merge(dis_counts_by_age, patients_counts_by_age, on='Age')
+
+# 각 나이대별 5% 이상 빈도인 질병만 필터링
+dis_counts_by_age['Percentage'] = (dis_counts_by_age['Count'] / dis_counts_by_age['PatientCount']) * 100
+filtered_dis_counts = dis_counts_by_age[dis_counts_by_age['Percentage'] >= 5]
+
+#Age를 인덱스, Disease를 열, Count를 값으로 사용
+pivoted_disease_counts = filtered_dis_counts.pivot_table(index='Disease', columns='Age', values='Count', aggfunc='sum', fill_value=0)
+
+#데이터 시각화
+plt.figure(figsize=(12, 8))
+sns.heatmap(pivoted_disease_counts, annot=True, fmt="d", cmap="Blues", cbar_kws={'label': 'Disease Count'})
+
+# 그래프
+plt.title('Disease Frequency by Age', fontsize=16)
+plt.xlabel('Age', fontsize=12)
+plt.ylabel('Disease', fontsize=12)
+plt.xticks(rotation=45)
+plt.tight_layout()
+
+plt.show()
+
+"""
+#Blood Pressure과 Cholesterol Level로 그룹화
+grouped_patient_d=patients_df.groupby(['Blood Pressure', 'Cholesterol Level'])
+for (bp, chol), group in grouped_patient_d:
+    print(f"Group: Blood Pressure = {bp}, Cholesterol Level = {chol}")
+    # Disease 빈도수 계산
+    disease_counts = group['Disease'].value_counts()
+
+"""
+
+
